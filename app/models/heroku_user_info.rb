a1d757be55ea2ffff4622173b3d7ec1cc167f026
Raul Murciano <raul@murciano.net>
2013-07-18 10:32:45 +0200

    Set username as full name by default

--- app/models/heroku_user_info.rb
+++ app/models/heroku_user_info.rb
@@ -23,7 +23,7 @@ class HerokuUserInfo < ActiveRecord::Base
   def self.create_from_heroku_data(heroku_uid, email)
     username = UsernameGenerator.generate(email)
     user_info = new(heroku_user_id: heroku_uid, screen_name: username)
-    user = User.create!(email: email, username: username, active: true)
+    user = User.create!(email: email, username: username, active: true, name: username)
     user_info.user = user
     user_info.save
     user_info
