e6f79d8045f0fcd49a827d089b870f4bcf27703d
Ryan Daigle <ryan.daigle@gmail.com>
2013-07-29 10:49:04 -0400

    Set worker db pool == worker concurrency

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,16 @@
 sidekiq_redis = { url: $redis.url, namespace: 'sidekiq' }
 
-Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
-Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
+Sidekiq.configure_server do |config|
+	config.redis = sidekiq_redis
+  database_url = ENV['DATABASE_URL']
+  sidekiq_concurrency = ENV['WORKER_CONCURRENCY'] || 5
+  if(database_url && sidekiq_concurrency)
+    Rails.logger.debug("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
+    ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}"
+    ActiveRecord::Base.establish_connection
+  end
+  Rails.logger.info("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+end
 
+Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
 Sidekiq.logger.level = Logger::WARN
