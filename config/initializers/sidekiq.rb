d24873f269e5a9f0205ff18d9f6f90fad34a3ab5
Ryan Daigle <ryan.daigle@gmail.com>
2013-08-01 09:43:51 -0400

    Add buffer to sidekiq worker pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,7 @@ sidekiq_redis = { url: $redis.url, namespace: 'sidekiq' }
 Sidekiq.configure_server do |config|
 	config.redis = sidekiq_redis
   database_url = ENV['DATABASE_URL']
-  sidekiq_concurrency = ENV['WORKER_CONCURRENCY'] || 5
+  sidekiq_concurrency = ENV['WORKER_CONCURRENCY'] ? (ENV['WORKER_CONCURRENCY'].to_i + 5) : 5
   if(database_url && sidekiq_concurrency)
     Rails.logger.debug("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
     ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}"
